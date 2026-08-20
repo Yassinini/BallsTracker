7525cc3ec13d0d5885a5c2d19e97c9a9cff752eb
Yassinini <yassinmehana2009@gmail.com>
2026-08-20 21:38:23 +0300

    Threshold code

---
+++ main.py
@@ -0,0 +1,42 @@
+import cv2 as cv
+import numpy as np
+
+vid_path = "/home/suupatruupa/Downloads/WhatsApp Video 2026-08-20 at 18.32.52.mp4"
+vid = cv.VideoCapture(vid_path)
+
+lower_lim = np.array([0, 0, 200])
+upper_lim = np.array([180, 40, 255])
+
+kernel = np.ones((3, 3), np.uint8)
+
+while True:
+    ok, frame = vid.read()
+    if not ok:
+        break
+
+    smol = cv.resize(frame, (1200, 800))
+    grey = cv.cvtColor(smol, cv.COLOR_BGR2GRAY)
+
+    _, mask = cv.threshold(grey, 150, 255, cv.THRESH_BINARY)
+    mask = cv.erode(mask, kernel, iterations=2)
+    mask = cv.dilate(mask, kernel, iterations=2)
+
+    y, x = np.where(mask == 255)
+
+    if len(x) > 0:
+        cx = np.mean(x)
+        cy = np.mean(y)
+
+        x_min, x_max = x.min(), x.max()
+        y_min, y_max = y.min(), y.max()
+        cv.rectangle(smol, (x_min, y_min), (x_max, y_max), (0, 255, 140), 2)
+        cv.circle(smol, (int(cx), int(cy)), 2, (180, 255, 40), 1)
+
+    #cv.imshow("frame", smol)
+    cv.imshow("mask", mask)
+
+    if cv.waitKey(1) & 0xFF == 27:
+        break
+
+vid.release()
+cv.destroyAllWindows()
